6fd61e62fead4f44ec0f680abf1e67114afdf869
anonymous-oracle <48692520+anonymous-oracle@users.noreply.github.com>
2021-11-12 11:57:18 +0530

    init with alembic

---
+++ alembic/versions/1f4ffad63990_create_posts_table.py
@@ -0,0 +1,24 @@
+"""create posts table
+
+Revision ID: 1f4ffad63990
+Revises: 
+Create Date: 2021-11-12 11:56:19.037506
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1f4ffad63990'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
